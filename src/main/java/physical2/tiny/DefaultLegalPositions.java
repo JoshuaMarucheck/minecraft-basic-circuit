6a2ef6cad5bf384a6e7724073772305361dae992
Joshua Marucheck <85257572+JoshuaMarucheck@users.noreply.github.com>
2022-04-28 00:17:06 -0400

    Made DefaultLegalPositions go in squares at first

--- src/main/java/physical2/tiny/DefaultLegalPositions.java
+++ src/main/java/physical2/tiny/DefaultLegalPositions.java
@@ -2,7 +2,9 @@ package physical2.tiny;
 
 import physical2.two.Point2D;
 
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 import static physical2.blocks.AbsolutePhysical3DMap2.mapY;
 import static physical2.blocks.SideMapping.Y_BUFFER;
@@ -11,6 +13,18 @@ import static physical2.blocks.SideMapping.Y_BUFFER;
  * In tiny coords
  */
 public class DefaultLegalPositions implements Iterable<Point2D> {
+  public static void main(String[] args) {
+    Set<Point2D> points = new HashSet<>();
+    Iterator<Point2D> iter = new DefaultLegalPositions().iterator();
+    for (int i = 0; i < WORLD_HEIGHT * WORLD_HEIGHT; i++) {
+      Point2D p = iter.next();
+      if (points.contains(p)) {
+        throw new IllegalStateException("Duplicate point " + p + " given");
+      }
+      points.add(p);
+    }
+  }
+
   private static final int WORLD_HEIGHT = 256;
 
   @Override
@@ -20,10 +34,12 @@ public class DefaultLegalPositions implements Iterable<Point2D> {
 
   private static class Iter implements Iterator<Point2D> {
     int x, y;
+    int localMax;
 
     public Iter() {
       x = 0;
       y = 0;
+      localMax = 0;
     }
 
     @Override
@@ -33,10 +49,20 @@ public class DefaultLegalPositions implements Iterable<Point2D> {
 
     @Override
     public Point2D next() {
-      y++;
-      if (mapY(y) + Y_BUFFER >= WORLD_HEIGHT) {
-        y = 0;
-        x++;
+      if (y == localMax) {
+        if (x == 0) {
+          localMax++;
+          x = localMax;
+          y = 0;
+        } else {
+          x--;
+        }
+      } else {
+        y++;
+        if (mapY(y) + Y_BUFFER >= WORLD_HEIGHT) {
+          y = 0;
+          x++;
+        }
       }
 
       return new Point2D(x, y);
