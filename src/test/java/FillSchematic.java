8a0d3e28c030683b76249691467cecc10e6bb72e
Joshua Marucheck <85257572+JoshuaMarucheck@users.noreply.github.com>
2022-04-29 18:10:14 -0400

    Allowed for different schematic names in FillSchematic

--- src/test/java/FillSchematic.java
+++ src/test/java/FillSchematic.java
@@ -3,14 +3,23 @@ import robot.SchematicFiller;
 
 import java.awt.*;
 import java.io.File;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 import static physical2.SchematicSplitter.MAX_SIZE;
 
 public class FillSchematic {
-  private static final File target = Paths.get(Constants.root).resolve("schematic").resolve("is_palindrome").toFile();
+  private static final Path parent = Paths.get(Constants.root).resolve("schematic");
 
   public static void main(String[] args) throws AWTException {
-    new SchematicFiller(MAX_SIZE).constructSchematic(target, "is_palindrome");
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Name of schematic: ");
+    String line = sc.nextLine();
+    if (line.equals("")) {
+      System.out.println("Defaulting to 'is_palindrome'");
+      line = "is_palindrome";
+    }
+    new SchematicFiller(MAX_SIZE).constructSchematic(parent.resolve(line).toFile(), line);
   }
 }
